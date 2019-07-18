44177967afb2e83d2f5ca3f4b9836ace3820d196
anthonyvigz <anthonyvigz@gmail.com>
2019-07-18 19:10:28 -0400

    added all dependencies

--- friends/src/index.js
+++ friends/src/index.js
@@ -3,10 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { applyMiddleware, createStore, compose } from "redux";
+import reducer from "./reducers/reducer";
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+
+
+const store = createStore(
+    reducer,
+    compose(
+      applyMiddleware(thunk, logger),
+      window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f,
+    )/* applyMiddleware goes here */
+);
+
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>,
     document.getElementById('root')
 );
