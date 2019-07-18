f5fc149a8d0fa6d615c757771d8c14b3571057fc
anthonyvigz <anthonyvigz@gmail.com>
2019-07-18 18:44:06 -0400

    created basic login form

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,9 +1,35 @@
 import React from 'react'
 
-const Login = props => {
-    return (
-        <h1>hey</h1>
-    )
+class Login extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            username: '',
+            password: ''
+        }
+    }
+    
+    // handles changing values in inputs
+    handleChange = event => {
+        event.preventDefault();
+
+        this.setState( { [event.target.name]: event.target.value })
+    }
+
+    // renders out the login form
+    render() {
+        const { username, password } = this.state
+        
+        return (
+            <div className="login">
+                <form>
+                    <input type="text" name="username" placeholder="Username" value={username} onChange={this.handleChange} />
+                    <input type="password" name="password" placeholder="Password" value={password} onChange={this.handleChange} />
+                    <button type="submit">Login</button>
+                </form>
+            </div>
+        )
+    }
 }
 
 export default Login;
