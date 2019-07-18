8870fb6cfb974a82024c22837654da767ba0203f
anthonyvigz <anthonyvigz@gmail.com>
2019-07-18 18:26:57 -0400

    setup login route

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import { Route } from 'react-router-dom'
+import Login from './components/Login'
 
 function App() {
   return (
     <div className="App">
-      
+      <Route exact path="/login" component={Login} />
     </div>
   );
 }
--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,2 +1,9 @@
 import React from 'react'
 
+const Login = props => {
+    return (
+        <h1>hey</h1>
+    )
+}
+
+export default Login;
