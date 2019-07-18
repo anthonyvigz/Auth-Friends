d380ef86b445ea34533bf4cf23c52c480f524099
anthonyvigz <anthonyvigz@gmail.com>
2019-07-18 19:37:02 -0400

    created basic login action to dispatch

---
+++ friends/src/actions/actions.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+
+export const LOGIN_START = 'LOGIN_START'
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
+export const LOGIN_FAILED = 'LOGIN_FAILED'
+
+
+
+export function login(username, password) {
+    return (dispatch) => {
+        dispatch({ type: LOGIN_START })
+
+        return axios.post('http://localhost:5000/', { username, password })
+            .then((response) => {
+                localStorage.setItem('token', response.data.token)
+                dispatch({ type: LOGIN_SUCCESS })
+            })
+            
+            .catch((error) => {
+                dispatch({ type: LOGIN_FAILED, error })
+            })
+    }
+}
---
+++ friends/src/reducers/reducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+    friends: [],
+    isLoading: false,
+    error: null
+}
+
+const reducer = (state = initialState, action) => {
+    return state;
+
+}
+
+export default reducer;
